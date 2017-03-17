7a6745ca18e6c58b795693e1a4a5795ccf3e775d
dmitryame <dmitry@custommode.com>
2017-03-17 15:25:42 -0400

    users migration added

--- migrations/1489695922327_user.js
+++ migrations/1489695922327_user.js
@@ -1,7 +1,13 @@
 exports.up = function(pgm) {
-
-};
+  pgm.createTable(
+    'users',
+    {
+      id: 'id',
+      email: {type: 'string', notNull: true, unique: true },
+      password: {type: 'string', notNull: true }
+    })
+}
 
 exports.down = function(pgm) {
-
+  pgm.dropTable('users')
 };
