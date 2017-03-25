75fb769710b9d7112e1e9f685db76b53bf88757c
dmitryame <dmitry@custommode.com>
2017-03-25 10:23:05 -0400

    jwt authorization works

--- config/passportStrategies.js
+++ config/passportStrategies.js
@@ -16,27 +16,23 @@ const jwtOptions = {
 
 // Create JWT Strategy
 const jwtLogin = new JwtStrategy(jwtOptions, async(payload, done) => {
-logger.debug("payload: ", payload)
-// return done(null, false)
-// })
-//   logger.debug("payload: ", payload)
-//   // See if the user email in the payload exists in our database
-//   // If it does, call 'done' with that user
-//   //Otherwise, call dobe without a user object
-//
-//   logger.debug("payload.sub: ", payload.sub)
-//
-//   try {
-//     let user = await User.findByEmail(payload.sub)
-//     if (user) {
-//       return done(null, user)
-//     } else {
-//       return done(null, false)
-//     }
-//   } catch (err) {
-//     logger.error("failed JWT Strategy: ", err)
-//     return done(err)
-//   }
+  logger.debug("payload: ", payload)
+  // See if the user email in the payload exists in our database
+  // If it does, call 'done' with that user
+  //Otherwise, call dobe without a user object
+  logger.debug("payload.sub: ", payload.sub)
+
+  try {
+    let user = await User.findByEmail(payload.sub)
+    if (user) {
+      return done(null, user)
+    } else {
+      return done(null, false)
+    }
+  } catch (err) {
+    logger.error("failed JWT Strategy: ", err)
+    return done(err)
+  }
 })
 
 // Tell passport to use this Strategy
