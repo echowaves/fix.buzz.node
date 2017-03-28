00df886e5afe79002d27b94d2bad6ae54b72fc4c
dmitryame <dmitry@custommode.com>
2017-03-28 15:14:48 -0400

    changing project structure to reflect api layer separate from react

--- config/routes.js
+++ config/routes.js
@@ -1,6 +1,6 @@
 import Router from 'koa-router'
 
-import indexController from '../src/api/controllers/index'
+import indexController from '../src/react/index'
 import AuthenticationController from '../src/api/controllers/authentication'
 
 require('./passportStrategies')
@@ -16,7 +16,7 @@ module.exports = function (app) {
   app.use(passport.initialize())
 
   router
-    .get('/',            requireAuth,       indexController.helloWorld)
+    .get('/', indexController)
     .post('/api/signin', requireSignin,     AuthenticationController.signin)
     .post('/api/signup',                    AuthenticationController.signup)
 
---
+++ src/models/user.js
@@ -0,0 +1,45 @@
+import Sequelize from 'sequelize'
+import {sequelize} from '../../config/consts'
+import logger from '../../lib/logger'
+import bcrypt from 'bcrypt-as-promised'
+
+var User = sequelize.define('user', {
+  email: {
+    type: Sequelize.STRING
+  },
+  password: {
+    type: Sequelize.STRING
+  }
+}, {
+  hooks: {
+    afterValidate: async(user, options) => {
+      logger.debug("after validate hashing user.password")
+      let hashed_password  = await bcrypt.hash(user.password, 10)
+      user.password = hashed_password
+    }
+  }
+});
+
+
+// Adding a class level method
+User.findByEmail = async email => {
+  return await User.findOne({
+    where: {
+      email: email
+    }
+  })
+}
+
+// Adding an instance level method
+User.Instance.prototype.comparePassword = async function(candidatePassword) {
+  try {
+    let isMatch = await bcrypt.compare(candidatePassword, this.password)
+    logger.debug("there is a match: ", isMatch)
+    return isMatch
+  } catch(err) {
+    logger.debug("failed to match the passwords: ", err)
+  }
+  return false
+}
+
+export default User
