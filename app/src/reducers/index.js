24aa11de072151ddebf9de3d4d5abf8fa0600e28
dmitryame <dmitry@custommode.com>
2017-04-29 18:14:59 -0400

    bootstrapping signin form

--- app/src/reducers/index.js
+++ app/src/reducers/index.js
@@ -1,21 +1,8 @@
-// import { combineReducers } from 'redux';
-// import { reducer as form } from 'redux-form';
-// // import authReducer from './auth_reducer';
-//
-// const rootReducer = combineReducers({
-//   form
-// });
-//
-// export default rootReducer;
+import { combineReducers } from 'redux'
+import { reducer as form } from 'redux-form'
 
+const rootReducer = combineReducers({
+  form
+});
 
-export default (state = 0, action) => {
-  switch (action.type) {
-    case 'INCREMENT':
-      return state + 1
-    case 'DECREMENT':
-      return state - 1
-    default:
-      return state
-  }
-}
+export default rootReducer
