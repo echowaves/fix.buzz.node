1f81a7ea4528eac6fb591bf0d587690c0fd51ac9
dmitryame <dmitry@custommode.com>
2017-04-29 22:59:28 -0400

    action placeholder

---
+++ app/src/actions/index.js
@@ -0,0 +1,20 @@
+
+
+
+export function signinUser({email, password}) {
+  return function(dispatch) {
+
+    // Submit email/password to the server
+
+    // if request is good...
+    // - Update state to indicate the user is authenticated
+    // - Save JWT token
+    // - Redirect to the route '/feature'
+
+    // if request is bad...
+    // - Show an error to the user
+
+  }
+
+
+}
--- app/src/index.js
+++ app/src/index.js
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import { BrowserRouter, Route } from 'react-router-dom'
+import reduxThunk from 'redux-thunk'
+
 
 import { createStore, applyMiddleware } from 'redux'
 
@@ -9,7 +11,7 @@ import App from './components/app'
 import Signin from './components/auth/signin'
 import reducers from './reducers'
 
-const createStoreWithMiddleware = applyMiddleware()(createStore)
+const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore)
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
